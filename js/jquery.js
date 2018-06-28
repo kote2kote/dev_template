0a88a6cf3686a36fc7536169e620fbb42a42f502
kote2 <kote2kote2@gmail.com>
2018-06-28 22:56:15 +0900

    add : jquery3.3.1を追加 / jsファイルをimport形式に変更 / 読み込みcssをstyle.css→style.min.cssに変更 / これら変更によるpackage.jsの変更

---
+++ js/jquery.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var $ = require('jQuery');
+
+$(function() {
+  
+  /*ここにjQueryを記述*/
+  $('h2').css('color', 'red');
+
+});
---
+++ js/module.js
@@ -0,0 +1,5 @@
+export let Person = "太郎";
+export let Dog = "オレオ";
+export let Cat = "ラテ";
+
+export default Person;
